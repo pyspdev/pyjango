30a42c416a69c15c63851da16da65598a9b243bf
pyspdev <pyspdev@gmail.com>
2025-09-09 14:12:53 -0600

    Update views.py
    update to views.py to fix template doesn´t exist and display home page

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,4 +4,9 @@ from .models import Project
 # Create your views here.
 def home(request):
     projects = Project.objects.all()
+     # Change 'home.html' to 'portfolio/home.html'
+     # This happens when you have a home.html file inside a subdirectory with the same
+     # name as the app, for example, portfolio/templates/portfolio/home.html. 
+     # It's a common convention in Django to prevent template name collisions 
+     # between different apps.
     return render(request,'portfolio/home.html', {'projects': projects})
