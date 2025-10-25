e5983ea05ae2beca24e16daca3ece50141a7e428
pyspdev <pyspdev@gmail.com>
2025-10-25 17:45:29 -0600

    FEAT: Resolviendo el error Unhandled Exception al cargar el módulo de URLs

--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -14,15 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import  include,path
-from django.conf.urls.static import static
-from django.conf import settings
-from portfolio import views
-
+#from django.contrib import admin
+#from django.urls import  include,path
+#from django.conf.urls.static import static
+#from django.conf import settings
+#from portfolio import views
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    
     # 1. Rutas para la página principal y el blog (probablemente ya están bien)
     path('', views.home, name='home'),
     path('blog/', include ('blog.urls')),
@@ -30,7 +32,12 @@ urlpatterns = [
     # de la aplicación 'portfolio' manejen todo lo que empiece con 'data-hub/'.
     # Si la ruta es solo 'data-hub/', esto apuntará a la URL principal de portfolio/urls.py
         # path('data_hub/', TemplateView.as_view(template_name='data_hub.html'), name='data_hub'),
-    path('data-hub/', include('portfolio.urls')), 
+    #path('data-hub/', include('portfolio.urls')), 
+
+    # Ruta para el Data Hub
+    # La ruta completa será '/data-hub/' debido al include en urls.py principal
+    path('', views.data_hub_view, name='data_hub')
+
 ]
 urlpatterns += static(settings.MEDIA_URL,
                       document_root=settings.MEDIA_ROOT)
